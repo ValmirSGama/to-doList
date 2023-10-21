515e7da8705add33ff15afabdab6960284893980
valmir sales gama <valmir_salesgama@hotmail.com>
2023-10-21 12:04:12 -0300

    first commit

---
+++ src/test/java/br/com/valmirsgama/todoList/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valmirsgama.todoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
